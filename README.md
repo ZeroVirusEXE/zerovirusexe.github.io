de19fc886cdbec7344981c99c6c461d74a16cc63
ZeroVirusEXE <ZeroVirusEXE@gmail.com>
2019-11-18 22:14:49 -0500

    .

---
+++ README.md
@@ -0,0 +1,2 @@
+## This is the build version of the main site for [zerovirusexe.github.io](https://zerovirusexe.github.io)  
+If you want to see the development environment for this project, go to this [link](https://github.com/ZeroVirusEXE/zvx)! (Also, the url [zerovirusexe.github.io/zvx](https://zerovirusexe.github.io/zvx) is the same as this build's webpage)  
---
+++ precache-manifest.e08a42cde12caa8afae6c0fcba0e0bb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80589276787773b0bfbcd74a7adef8ac",
+    "url": "/zvx/index.html"
+  },
+  {
+    "revision": "268cf0d06fbe32eccf4e",
+    "url": "/zvx/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8e6848944fdef40e5128",
+    "url": "/zvx/static/js/2.2078fd59.chunk.js"
+  },
+  {
+    "revision": "268cf0d06fbe32eccf4e",
+    "url": "/zvx/static/js/main.026c6898.chunk.js"
+  },
+  {
+    "revision": "ebb1447de7741de3f77a",
+    "url": "/zvx/static/js/runtime-main.6614e75e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/zvx/static/media/logo.25bf045c.svg"
+  }
+]);
